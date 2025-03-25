eddf15eed717776e71ce698f454de28594bbac7d
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-03-26 01:15:51 +0500

    Update .allo.js

--- plugins/.allo.js
+++ plugins/.allo.js
@@ -8,3 +8,56 @@ const path = require('path');
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions2');
 
 const prefix = config.PREFIX;
+
+cmd({
+    pattern: "lol",
+    desc: "Get view once.",
+    category: "owner",
+    react: "👀",
+    filename: __filename
+}, async (conn, mek, m, { isReply, quoted, reply }) => {
+    try {
+        // Check if the message is a view once message
+        if (!m.quoted) return reply("Please reply to a view once message!");
+
+        const qmessage = m.message.extendedTextMessage.contextInfo.quotedMessage;
+
+            const mediaMessage = qmessage.imageMessage ||
+                                qmessage.videoMessage ||
+                                qmessage.audioMessage;
+
+            if (!mediaMessage?.viewOnce) {
+              return reply("_Not A VV message")
+            }
+
+            try {
+            const buff = await m.quoted.getbuff
+            const cap = mediaMessage.caption || '';
+
+            if (mediaMessage.mimetype.startsWith('image')) {
+                  await conn.sendMessage(m.chat, {
+                  image: buff,
+                 caption: cap
+         }); 
+            } else if (mediaMessage.mimetype.startsWith('video')) {
+              await conn.sendMessage(m.chat, {
+                  video: buff,
+                 caption: cap
+         }); 
+            } else if (mediaMessage.mimetype.startsWith('audio')) {
+              await conn.sendMessage(m.chat, {
+                  audio: buff,
+                  ptt: mediaMessage.ptt || false
+         }); 
+            } else {
+              return reply("_*Unkown/Unsupported media*_");
+        }
+    } catch (error) {
+        console.error(error);
+        reply(`${error}`)
+    }
+} catch (e) {
+  console.error(e);
+        reply(`${e}`);
+}
+});
