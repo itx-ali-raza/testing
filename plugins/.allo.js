40398786b75c1f0b68555604c0bc3f1937373db0
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-03-25 22:43:56 +0500

    Create .allo.js

---
+++ plugins/.allo.js
@@ -0,0 +1,63 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { proto, downloadContentFromMessage } = require('baileys');
+const { sms,downloadMediaMessage } = require('../lib/msg');
+const fs = require('fs');
+const exec = require('child_process');
+const path = require('path');
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson } = require('../lib/functions');
+
+const prefix = config.PREFIX;
+
+cmd({
+    pattern: "lol",
+    desc: "Get view once.",
+    category: "owner",
+    react: "👀",
+    filename: __filename
+}, async (conn, mek, m, { isReply, quoted, reply }) => {
+    try {
+        // Check if the message is a view once message
+        if (!m.quoted) return reply("Please reply to a view once message!");
+
+        const qmessage = m.message.extendedTextMessage.contextInfo.quotedMessage;
+
+            const mediaMessage = qmessage.imageMessage ||
+                                qmessage.videoMessage ||
+                                qmessage.audioMessage;
+
+            if (!mediaMessage?.viewOnce) {
+              return reply("_Not A VV message")
+            }
+
+            try {
+            const buff = await m.quoted.getbuff
+            const cap = mediaMessage.caption || '';
+
+            if (mediaMessage.mimetype.startsWith('image')) {
+                  await conn.sendMessage(m.chat, {
+                  image: buff,
+                 caption: cap
+         }); 
+            } else if (mediaMessage.mimetype.startsWith('video')) {
+              await conn.sendMessage(m.chat, {
+                  video: buff,
+                 caption: cap
+         }); 
+            } else if (mediaMessage.mimetype.startsWith('audio')) {
+              await conn.sendMessage(m.chat, {
+                  audio: buff,
+                  ptt: mediaMessage.ptt || false
+         }); 
+            } else {
+              return reply("_*Unkown/Unsupported media*_");
+        }
+    } catch (error) {
+        console.error(error);
+        reply(`${error}`)
+    }
+} catch (e) {
+  console.error(e);
+        reply(`${e}`);
+}
+});
