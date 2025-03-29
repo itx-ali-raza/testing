6721d2d08a8ac8a3e2f781dca6274f35d284de92
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-03-29 06:34:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const {
   const qrcode = require('qrcode-terminal')
   const StickersTypes = require('wa-sticker-formatter')
   const util = require('util')
-  const { sms, downloadMediaMessage, AntiDelete } = require('./lib')
+  const { sms, downloadMediaMessage } = require('./lib/msg2');
   const FileType = require('file-type');
   const axios = require('axios')
   const { File } = require('megajs')
