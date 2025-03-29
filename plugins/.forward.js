c0e80bafe68967e55c1d989911862a5ad0f7870b
𓆩ု᪳𝐀𝐋𝐈 𝐈𝐍𝅦𝐒i͜͡𝐃𝐄ှ᪳𓆪 <alirazalashari457@gmail.com>
2025-03-29 05:51:29 +0500

    Update .forward.js

--- plugins/.forward.js
+++ plugins/.forward.js
@@ -12,38 +12,14 @@ cmd(
         category: "main",
         use: ".ping",
         filename: __filename,
-    },
-    async (conn, mek, m, { from, quoted, reply }) => {
-        try {
-            const start = new Date().getTime();
+  },
+  async (message) => {
+    const start = performance.now();
+    const msg = await message.send('Testing Speed...');
+    const end = performance.now();
+    await msg.edit(`\`\`\`Pong! ${(end - start).toFixed(1)} ms\`\`\``);
+  }
+);
 
-        const reactionEmojis = ['🔥', '🔮', '🌩️', '👻', '🍁', '🐍', '🎋', '🎐', '🪸', '📍', '👑', '🌀', '🪄'];
-        const textEmojis = ['🪀', '🪂', '⚡️', '🚀', '🏎️', '🚁', '🌀', '📟', '🎲', '✨'];
-
-        const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
-        let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
-
-        // Ensure reaction and text emojis are different
-        while (textEmoji === reactionEmoji) {
-            textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
-        }
-
-        // Send reaction using conn.sendMessage()
-        await conn.sendMessage(from, {
-            react: { text: textEmoji, key: mek.key }
-        });
-            // Start ping measurement
-            const startTime = Date.now();
-            await conn.sendMessage(from, { text: "> pinning from express server" });
-            const endTime = Date.now();
-            const ping = endTime - startTime;
-
-            // Ping response
-            await conn.sendMessage(
-                from,
-                { text: ` *${reactionEmoji} : ${ping}ms*` }, { quoted: message })
-    } catch (e) {
-        console.log(e)
-        reply(`${e}`)
-    }
-})
+bot(
+    {
